169869faf3672b2eef2390d0987b0b7a96b6ddae
Yahya Fati <yfati037@gmail.com>
2022-08-20 18:29:57 +0300

    initial commit

---
+++ src/main/kotlin/com/example/quizbuilder/QuizBuilderApplication.kt
@@ -0,0 +1,11 @@
+package com.example.quizbuilder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuizBuilderApplication
+
+fun main(args: Array<String>) {
+    runApplication<QuizBuilderApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/quizbuilder/QuizBuilderApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.quizbuilder
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QuizBuilderApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
