6e14782859693139394cf17014140dccbe0201c0
Yahya Fati <yfati037@gmail.com>
2022-08-29 15:02:50 +0300

    removed answer from json response

--- src/main/kotlin/com/yahya/quizbuilderkt/model/Choice.kt
+++ src/main/kotlin/com/yahya/quizbuilderkt/model/Choice.kt
@@ -1,6 +1,7 @@
 package com.yahya.quizbuilderkt.model
 
 import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
 import javax.persistence.*
 import javax.validation.constraints.NotBlank
 
@@ -14,7 +15,7 @@ class Choice(
     @Column(nullable = false)
     var text: String = "",
 
-//    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     var answer: Boolean = false,
 
     @ManyToOne
